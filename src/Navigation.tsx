5ca074e96d98b103397424096a59d10b7f2354d3
Simon <sstur@me.com>
2023-02-02 14:30:41 -0800

    Add LoginScreen

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -3,14 +3,16 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { HomeScreen } from './screens/HomeScreen';
+import { LoginScreen } from './screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export function Navigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
