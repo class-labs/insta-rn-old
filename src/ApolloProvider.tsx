0c65c2a940bc36f6fa677468d0d1632a37efa1d5
Simon <sstur@me.com>
2023-02-03 10:41:06 -0800

    [temp] Hard-code auth token in Apollo headers

--- src/ApolloProvider.tsx
+++ src/ApolloProvider.tsx
@@ -1,12 +1,27 @@
 import { ReactNode } from 'react';
 import {
   ApolloClient,
-  InMemoryCache,
   ApolloProvider as Provider,
+  createHttpLink,
+  InMemoryCache,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: process.env.GRAPHQL_API + '/graphql',
+});
+
+const authLink = setContext(() => {
+  const token = 'q0486hx91';
+  return {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: process.env.GRAPHQL_API + '/graphql',
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
