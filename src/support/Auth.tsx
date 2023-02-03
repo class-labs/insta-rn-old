204cf10e326c8b41d54bb1a80d1dfa2b2241cead
Simon <sstur@me.com>
2023-02-03 11:19:07 -0800

    Persist auth token to AsyncStorage

--- src/support/Auth.tsx
+++ src/support/Auth.tsx
@@ -2,10 +2,18 @@ import {
   createContext,
   ReactNode,
   useContext,
+  useEffect,
   useMemo,
   useRef,
   useState,
 } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const STORAGE_KEY = '@auth-token';
+
+async function getSavedAuthToken() {
+  return await AsyncStorage.getItem(STORAGE_KEY);
+}
 
 type Context = {
   getAuthToken: () => string | null;
@@ -15,16 +23,38 @@ type Context = {
 const AuthContext = createContext<Context | null>(null);
 
 export function AuthProvider(props: { children: ReactNode }) {
+  const [isLoading, setLoading] = useState(true);
   const [authToken, setAuthToken] = useState<string | null>(null);
   const authTokenRef = useRef(authToken);
   authTokenRef.current = authToken;
   const context = useMemo<Context>(
     () => ({
       getAuthToken: () => authTokenRef.current,
-      setAuthToken: (token: string | null) => setAuthToken(token),
+      setAuthToken: (token: string | null) => {
+        setAuthToken(token);
+        // Update the device storage
+        if (token === null) {
+          AsyncStorage.removeItem(STORAGE_KEY);
+        } else {
+          AsyncStorage.setItem(STORAGE_KEY, token);
+        }
+      },
     }),
     [],
   );
+
+  // Load the previously saved auth token from device storage
+  useEffect(() => {
+    getSavedAuthToken().then((token) => {
+      setAuthToken(token);
+      setLoading(false);
+    });
+  }, []);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
     <AuthContext.Provider value={context}>
       {props.children}
