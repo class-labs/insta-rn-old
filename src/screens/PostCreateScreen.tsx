37eb38c5b5f8dae42e56a883a88baf551167c307
Simon <sstur@me.com>
2023-02-03 16:17:12 -0800

    Implement uploading

--- src/screens/PostCreateScreen.tsx
+++ src/screens/PostCreateScreen.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { Pressable } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { RouteProp, useRoute } from '@react-navigation/native';
-import { Image, Text, YStack } from 'tamagui';
+import { Button, Image, Text, YStack } from 'tamagui';
 import { RootStackParamList } from '../types/Navigation';
 
 type ResolvedRouteProp = RouteProp<RootStackParamList, 'PostCreate'>;
@@ -13,17 +13,23 @@ type UploadResponse = {
   url: string;
 };
 
+async function uriToBlob(imageUri: string) {
+  const response = await fetch(imageUri);
+  return await response.blob();
+}
+
 async function uploadImage(
   imageUri: string,
   mimeType = 'image/jpeg',
 ): Promise<UploadResponse> {
+  const blob = await uriToBlob(imageUri);
   const response = await fetch(API_BASE + '/images', {
     method: 'POST',
     headers: {
       'content-type': 'application/octet-stream',
       'content-disposition': mimeType,
     },
-    body: imageUri,
+    body: blob,
   });
   return await response.json();
 }
@@ -34,34 +40,45 @@ export function PostCreateScreen() {
     return route.params.capturedPhoto?.uri ?? null;
   });
   const [uploadedImage, setUploadedImage] = useState<string | null>(null);
-  useEffect(() => {
-    if (route.params.openPicker) {
-      // TODO: Open the photo picker
+
+  const startUpload = async (uri: string) => {
+    const result = await uploadImage(uri);
+    setUploadedImage(result.url);
+  };
+
+  const openImagePicker = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    setUploadedImage(null);
+    if (!result.canceled) {
+      const asset = result.assets[0];
+      if (asset) {
+        setImageUri(asset.uri);
+        startUpload(asset.uri);
+      }
     }
-  }, [route]);
+  };
+
+  useEffect(() => {
+    const { openPicker, capturedPhoto } = route.params;
+    if (capturedPhoto) {
+      startUpload(capturedPhoto.uri);
+    }
+    if (openPicker) {
+      openImagePicker();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <YStack flex={1} backgroundColor="white" space={12}>
       <Pressable
         style={({ pressed }) => (pressed ? { opacity: 0.5 } : undefined)}
-        onPress={async () => {
-          let result = await ImagePicker.launchImageLibraryAsync({
-            mediaTypes: ImagePicker.MediaTypeOptions.All,
-            allowsEditing: true,
-            aspect: [4, 3],
-            quality: 1,
-          });
-          console.log(result);
-          setUploadedImage(null);
-          if (!result.canceled) {
-            const asset = result.assets[0];
-            if (asset) {
-              setImageUri(asset.uri);
-              const { url } = await uploadImage(asset.uri);
-              setUploadedImage(url);
-            }
-          }
-        }}
+        onPress={() => openImagePicker()}
       >
         <YStack
           width="100%"
@@ -77,8 +94,17 @@ export function PostCreateScreen() {
           )}
         </YStack>
       </Pressable>
-      <Text>Chosen image: {String(imageUri)}</Text>
-      <Text>Uploaded image: {String(uploadedImage)}</Text>
+      <YStack px={16} space={12}>
+        <Text>Chosen image: {String(imageUri)}</Text>
+        <Text>Uploaded image: {String(uploadedImage)}</Text>
+        <Button
+          onPress={() => {
+            // TODO: GraphQL Mutation
+          }}
+        >
+          Submit
+        </Button>
+      </YStack>
     </YStack>
   );
 }
