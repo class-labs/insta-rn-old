089344331b254af9cfe51542a57d8a2b716d6586
Simon <sstur@me.com>
2023-02-03 16:07:04 -0800

    Implement image picking

--- src/screens/PostCreateScreen.tsx
+++ src/screens/PostCreateScreen.tsx
@@ -1,17 +1,84 @@
+import { useEffect, useState } from 'react';
+import { Pressable } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { RouteProp, useRoute } from '@react-navigation/native';
-import { Text } from 'tamagui';
+import { Image, Text, YStack } from 'tamagui';
 import { RootStackParamList } from '../types/Navigation';
 
-type RsolvedRouteProp = RouteProp<RootStackParamList, 'PostCreate'>;
+type ResolvedRouteProp = RouteProp<RootStackParamList, 'PostCreate'>;
+
+const API_BASE = process.env.GRAPHQL_API;
+
+type UploadResponse = {
+  url: string;
+};
+
+async function uploadImage(
+  imageUri: string,
+  mimeType = 'image/jpeg',
+): Promise<UploadResponse> {
+  const response = await fetch(API_BASE + '/images', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/octet-stream',
+      'content-disposition': mimeType,
+    },
+    body: imageUri,
+  });
+  return await response.json();
+}
 
 export function PostCreateScreen() {
-  const route = useRoute<RsolvedRouteProp>();
-  if (route.params.openPicker) {
-    // TODO: Open the photo picker automatically
-    // You might need to useEffect()
-  }
-  // Render the form. If there's no photo to render (which in this case there won't be)
-  // then just have a button to open the picker
-  // See: https://docs.expo.dev/versions/latest/sdk/imagepicker/
-  return <Text>Create form will go here</Text>;
+  const route = useRoute<ResolvedRouteProp>();
+  const [imageUri, setImageUri] = useState<string | null>(() => {
+    return route.params.capturedPhoto?.uri ?? null;
+  });
+  const [uploadedImage, setUploadedImage] = useState<string | null>(null);
+  useEffect(() => {
+    if (route.params.openPicker) {
+      // TODO: Open the photo picker
+    }
+  }, [route]);
+
+  return (
+    <YStack flex={1} backgroundColor="white" space={12}>
+      <Pressable
+        style={({ pressed }) => (pressed ? { opacity: 0.5 } : undefined)}
+        onPress={async () => {
+          let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1,
+          });
+          console.log(result);
+          setUploadedImage(null);
+          if (!result.canceled) {
+            const asset = result.assets[0];
+            if (asset) {
+              setImageUri(asset.uri);
+              const { url } = await uploadImage(asset.uri);
+              setUploadedImage(url);
+            }
+          }
+        }}
+      >
+        <YStack
+          width="100%"
+          aspectRatio={1}
+          backgroundColor="#dcdcdc"
+          justifyContent="center"
+          alignItems="center"
+        >
+          {imageUri ? (
+            <Image src={imageUri} width="100%" height="auto" aspectRatio={1} />
+          ) : (
+            <Text>Press to choose an image</Text>
+          )}
+        </YStack>
+      </Pressable>
+      <Text>Chosen image: {String(imageUri)}</Text>
+      <Text>Uploaded image: {String(uploadedImage)}</Text>
+    </YStack>
+  );
 }
