afb0dee0116656ce86907d604267498f447dbf1f
Simon <sstur@me.com>
2023-05-18 16:44:22 -0700

    Simplify Auth

--- src/support/Auth.tsx
+++ src/support/Auth.tsx
@@ -1,71 +1,37 @@
-import {
-  createContext,
-  ReactNode,
-  useContext,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const STORAGE_KEY = '@auth-token';
 
-async function getSavedAuthToken() {
-  return await AsyncStorage.getItem(STORAGE_KEY);
+let authToken: string | null = null;
+
+function getAuthToken() {
+  return authToken;
 }
 
-type Context = {
-  getAuthToken: () => string | null;
-  setAuthToken: (token: string | null) => void;
-};
-
-const AuthContext = createContext<Context | null>(null);
+function setAuthToken(token: string | null) {
+  authToken = token;
+  if (token === null) {
+    AsyncStorage.removeItem(STORAGE_KEY);
+  } else {
+    AsyncStorage.setItem(STORAGE_KEY, token);
+  }
+}
 
 export function AuthProvider(props: { children: ReactNode }) {
   const [isLoading, setLoading] = useState(true);
-  const [authToken, setAuthToken] = useState<string | null>(null);
-  const authTokenRef = useRef(authToken);
-  authTokenRef.current = authToken;
-  const context = useMemo<Context>(
-    () => ({
-      getAuthToken: () => authTokenRef.current,
-      setAuthToken: (token: string | null) => {
-        setAuthToken(token);
-        // Update the device storage
-        if (token === null) {
-          AsyncStorage.removeItem(STORAGE_KEY);
-        } else {
-          AsyncStorage.setItem(STORAGE_KEY, token);
-        }
-      },
-    }),
-    [],
-  );
-
-  // Load the previously saved auth token from device storage
   useEffect(() => {
-    getSavedAuthToken().then((token) => {
-      setAuthToken(token);
+    AsyncStorage.getItem(STORAGE_KEY).then((token) => {
+      authToken = token;
       setLoading(false);
     });
   }, []);
-
   if (isLoading) {
     return null;
   }
-
-  return (
-    <AuthContext.Provider value={context}>
-      {props.children}
-    </AuthContext.Provider>
-  );
+  return <>{props.children}</>;
 }
 
 export function useAuth() {
-  const context = useContext(AuthContext);
-  if (!context) {
-    throw new Error('useAuth must be within AuthProvider');
-  }
-  return context;
+  return { getAuthToken, setAuthToken };
 }
